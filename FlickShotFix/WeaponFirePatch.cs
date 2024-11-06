96f612259a74acf51805b44c8b1367a5fe929551
Dinorush <onparnorthstar@gmail.com>
2024-11-06 00:02:49 -0500

    Add project files.

---
+++ FlickShotFix/WeaponFirePatch.cs
@@ -0,0 +1,22 @@
+﻿using Gear;
+using HarmonyLib;
+using Player;
+
+namespace FlickShotFix
+{
+    [HarmonyPatch]
+    internal static class WeaponFirePatch
+    {
+        private static FPSCamera? _camera;
+
+        [HarmonyPatch(typeof(Shotgun), nameof(Shotgun.Fire))]
+        [HarmonyPatch(typeof(BulletWeapon), nameof(BulletWeapon.Fire))]
+        [HarmonyPrefix]
+        private static void PreFire()
+        {
+            if (_camera == null)
+                _camera = PlayerManager.GetLocalPlayerAgent().FPSCamera;
+            _camera.UpdateCameraRay();
+        }
+    }
+}
